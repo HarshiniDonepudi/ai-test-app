7bc5095d7d936a6f748d4dfe4e6382d76e537618
Harshini <harshudonepudi@gmail.com>
2025-11-18 18:18:36 -0500

    Fix GPT-5.1 parameter: use max_completion_tokens instead of max_tokens

--- api/index.js
+++ api/index.js
@@ -192,7 +192,7 @@ Important:
           ]
         }
       ],
-      max_tokens: 2000,
+      max_completion_tokens: 2000,
       temperature: 0.3
     });
     console.log('OpenAI API call successful');
